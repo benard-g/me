b3506b626ca51de9d226793c1bc63c91eca5c849
Guillaume Benard <guillaume@benard.io>
2020-09-30 15:55:58 +0200

    Customize React project

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from './App.module.css';
+import logo from './logo.svg';
+
+function App(): React.ReactElement {
+  return (
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt="logo" />
+        <p>
+          Edit <code>src/app/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles['App-link']}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
